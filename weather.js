1008c48ca52682c43049783648e6d2961e40382f
theomvirsingh <omvirsingh9062@gmail.com>
2024-01-28 20:14:13 +0530

    Weather API Project First Commit

---
+++ weather.js
@@ -0,0 +1,167 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+//Variable Needed
+let currentTab = userTab;
+const API_KEY = "a02f0ea1afdfd83d62b024b4d824a87d";
+currentTab.classList.add("current-tab");
+getfromSessionStorage();
+
+function switchTab(clickedTab){
+    if(clickedTab != currentTab){
+        currentTab.classList.remove("current-tab");
+        currentTab = clickedTab;
+        currentTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")){
+            //KYA SEACHFORM WALA CONTAINER INVISIBLE HAI, IF YES THEN MAKE IT VISIBLE
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+        else{
+            // M PHLE SEACH WALE TAB PE THA AB yourWeather WALA TAB VISBILE KRNA HAI
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            //AB M yourWeather  WALE TAB PE HU TO WEATHER DISPLAY KRNA PADEGA ISLIYE LOCAL STORAGE CHECK KRO 
+            //IF WE HAVE SAVED OUR LOCATION COORDINATES 
+            getfromSessionStorage();
+        }
+    }
+}
+
+userTab.addEventListener("click", () => {
+    //PASSED CLICKED TAB AS INPUT
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    switchTab(searchTab);
+});
+
+//checks if coordinates are already present in session storage
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates){
+        //agr user local coordinates  nhi mile 
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates){
+    const {lat, lon} = coordinates;
+    //make grantaccess wala page inactive
+    grantAccessContainer.classList.remove("active");
+
+    //make loading screen visible
+    loadingScreen.classList.add("active");
+
+    //API call
+    try{
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        );
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+        //
+    }
+}
+
+function renderWeatherInfo(weatherInfo){
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windSpeed = document.querySelector("[data-windSpeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    console.log(weatherInfo);
+
+    //fetching values and adding them to variables made
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windSpeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
+}
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        //
+    }
+}
+
+function showPosition(position){
+    const userCoordinates = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
+    fetchUserWeatherInfo(userCoordinates);
+}
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click",getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let cityName = searchInput.value;
+
+    if(cityName === ""){
+        return;
+    }
+    else{
+        fetchSearchWeatherInfo(cityName);
+    }
+})
+
+async function fetchSearchWeatherInfo(city){
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+    
+    try{
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(error) {
+            loadingScreen.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            showCityNotFoundImage();
+    }
+}
+
+function showCityNotFoundImage() {
+    var notFoundImage = document.getElementsByClassName(".notFoundImage");
+    notFoundImage.classList.add("active");
+}
